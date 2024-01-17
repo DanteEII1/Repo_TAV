918a81da42e6502172cc3dde2c3d0c8fa15da6fa
DanteEII1 <155745444+DanteEII1@users.noreply.github.com>
2024-01-17 00:44:31 -0300

    Entrega Experiencia2

---
+++ js/register.js
@@ -0,0 +1,60 @@
+$(function() 
+{
+  $("#mi-Formulario").validate({
+       rules: {
+              nom: {
+                  required:true,
+              },
+              correo: {
+                  required: true,
+                  email: true,
+              },
+              pass:{
+                  required:true,
+              },
+              fono: {
+                  required:true,
+                  number:true,
+              },
+              fecha: {
+                  required:true,
+                  
+              },
+              pass2: {
+                  required: true,
+                  equalTo: "#pass",
+              }   
+              
+          }, //rules
+      messages: {
+
+          nom:{
+              required: 'Ingrese nombre del usuario',
+              minlength: 'Caracteres insuficientes'
+          },
+          correo: {
+              required: 'Ingresa tu correo electrónico',
+              email: 'Formato de correo no válido'
+          },
+          pass: {
+              required: 'Ingresa una contraseña',
+              minlength: 'Caracteres insuficientes'
+          },
+          fono:{
+              required: 'Ingrese un número de celular',
+              minlength: 'Cantidad de digitos insuficiente'
+          },
+          fecha:{
+              required: 'Seleccione una fecha válida',
+              min: 'Fecha no corresponde'
+          },
+          pass2: {
+              required: 'Reingresa la contraseña',
+              equalTo: 'Las contraseñas ingresadas no coinciden',
+              minlength: 'Caracteres insuficientes'
+
+          }
+      }//messages
+  }); //$('#mi-formulario').validate
+}); 
+
